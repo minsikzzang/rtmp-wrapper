bab0e30174bdbff8daada842d5622da37b87926b
minsikzzang <minsikzzang@gmail.com>
2013-10-01 09:56:27 +0100

    some added

--- rtmp-wrapperTests/rtmp_wrapperTests.m
+++ rtmp-wrapperTests/rtmp_wrapperTests.m
@@ -7,6 +7,9 @@
 //
 
 #import <XCTest/XCTest.h>
+// #import "RtmpWrapper.h"
+#import "rtmp.h"
+// #import "sha.h"
 
 @interface rtmp_wrapperTests : XCTestCase
 
@@ -14,21 +17,33 @@
 
 @implementation rtmp_wrapperTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)setUp {
+  [super setUp];
+  // Put setup code here. This method is called before the invocation of each
+  // test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+- (void)tearDown {
+  // Put teardown code here. This method is called after the invocation of
+  // each test method in the class.
+  [super tearDown];
 }
 
+- (void)testSetupUrl {
+  // RtmpWrapper *rtmp = [[RtmpWrapper alloc] init];
+  // SHA_CTX *ctx = NULL;
+  // SHA1_Init(ctx);
+  // [rtmp release];
+  // RTMP *r = RTMP_Alloc();
+  // RTMP_Init(r);
+  
+}
+
+/*
 - (void)testExample
 {
     XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
 }
+*/
 
 @end
